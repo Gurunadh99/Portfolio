232e7146414ed64a88b7cd9f70e9040155f01e21
Gurunadh99 <147463490+Gurunadh99@users.noreply.github.com>
2025-12-03 13:06:36 +0530

    Create skills.js

---
+++ src/lib/skills.js
@@ -0,0 +1,7 @@
+const skills = [
+  { name: 'Python' }, { name: 'TensorFlow' }, { name: 'PyTorch' },
+  { name: 'OpenCV' }, { name: 'AWS' }, { name: 'scikit-learn' },
+  { name: 'NLP' }, { name: 'LLMs' }, { name: 'SQL' }
+]
+
+export default skills
