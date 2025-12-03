6dfc3ea489b6481a0a055f04666995fb147e2392
Gurunadh99 <147463490+Gurunadh99@users.noreply.github.com>
2025-12-03 12:08:50 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // IMPORTANT: set basePath/assetPrefix to match repo name for GitHub Pages
+  basePath: '/Portfolio',
+  assetPrefix: '/Portfolio',
+  // static export compatibility
+  trailingSlash: true
 }
 
 module.exports = nextConfig
